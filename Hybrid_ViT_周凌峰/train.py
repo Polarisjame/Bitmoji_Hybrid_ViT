3280bb6eb01a8bb64e13f22c120d360a462ae889
Polarisjame <3464790978@qq.com>
2022-12-20 13:01:10 +0800

    add GetModelSize()

--- Hybrid_ViT_周凌峰/train.py
+++ Hybrid_ViT_周凌峰/train.py
@@ -25,6 +25,20 @@ torch.manual_seed(seed)
 np.random.seed(seed)
 random.seed(seed)
 
+def getModelSize(model):
+    param_size = 0
+    param_sum = 0
+    for param in model.parameters():
+        param_size += param.nelement() * param.element_size()
+        param_sum += param.nelement()
+    buffer_size = 0
+    buffer_sum = 0
+    for buffer in model.buffers():
+        buffer_size += buffer.nelement() * buffer.element_size()
+        buffer_sum += buffer.nelement()
+    all_size = (param_size + buffer_size) / 1024 / 1024
+    print('模型总大小为：{:.3f}MB'.format(all_size))
+    return param_size, param_sum, buffer_size, buffer_sum, all_size
 
 def evaluate(model, data, lossF, device):
     val_targ = []
@@ -154,9 +168,10 @@ def main(args):
     data = VITSet(args)
     data.setup()
     device = "cuda" if torch.cuda.is_available() and args.use_cuda else "cpu"
-    # model = VIT(args).to(device)
+    model = VIT(args).to(device)
+    print(getModelSize(model))
     # model = Res34(args, 3, 2).to(device)
-    model = CNN(args, 3, 2).to(device)
+    # model = CNN(args, 3, 2).to(device)
     train(args, model, data, device)
     # test(args, model, data, device)
 
